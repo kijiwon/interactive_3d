793e6fd8dff8634f8f459f9284326e08985d66ff
kijiwon <kiji1313@gmail.com>
2025-08-01 01:45:26 +0900

    MeshObject 생성

---
+++ src/MeshObject.js
@@ -0,0 +1,26 @@
+import { Mesh, BoxGeometry, MeshLambertMaterial, DoubleSide } from "three";
+
+// MeshObject 생성 클래스
+export class MeshObject {
+  // class의 인스턴스 객체가 생성될 때마다 실행(초기화 역활)
+  constructor(info) {
+    this.name = info.name;
+    this.width = info.width || 1;
+    this.height = info.height || 1;
+    this.depth = info.depth || 1;
+    this.color = info.color || "white";
+    this.x = info.x || 0;
+    this.y = info.y || this.height / 2; // 모든 mesh는 지면에 닿아있어야함
+    this.z = info.z || 0;
+
+    const geometry = new BoxGeometry(this.width, this.height, this.depth);
+    const material = new MeshLambertMaterial({
+      color: this.color,
+      side: DoubleSide,
+    });
+
+    this.mesh = new Mesh(geometry, material);
+    this.mesh.position.set(this.x, this.y, this.z);
+    info.scene.add(this.mesh);
+  }
+}
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import { MeshObject } from "./MeshObject";
 
 // renderer
 const canvas = document.querySelector("#three-canvas");
@@ -15,7 +16,7 @@ renderer.shadowMap.enabled = true;
 
 // scene
 const scene = new THREE.Scene();
-scene.background = new THREE.Color("orange");
+scene.background = new THREE.Color("white");
 
 // camera
 const camera = new THREE.PerspectiveCamera(
@@ -31,50 +32,36 @@ scene.add(camera);
 const controls = new OrbitControls(camera, renderer.domElement);
 
 // light
-const ambientLight = new THREE.AmbientLight("white", 3);
-scene.add(ambientLight);
+const ambientLight = new THREE.AmbientLight("white", 1);
+const pointLight = new THREE.PointLight("white", 100, 100);
+pointLight.castShadow = true;
+pointLight.position.y = 10;
 
-const directionalLight = new THREE.DirectionalLight("white", 3);
-directionalLight.position.set(-3, 5, 1);
-directionalLight.castShadow = true;
-scene.add(directionalLight);
+scene.add(ambientLight, pointLight);
 
 // mesh
-const boxGeometry = new THREE.BoxGeometry(2, 2, 2);
-const boxMaterial = new THREE.MeshLambertMaterial({
-  color: "firebrick",
-  side: THREE.DoubleSide,
-});
-const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial);
-boxMesh.castShadow = true;
-boxMesh.position.y = 1;
-
-scene.add(boxMesh);
-
-const groundGeometry = new THREE.PlaneGeometry(10, 10);
-const groundMaterial = new THREE.MeshLambertMaterial({
+const ground = new MeshObject({
+  scene,
+  name: "ground",
+  width: 50,
+  height: 0.1,
+  depth: 50,
   color: "#092e66",
-  side: THREE.DoubleSide,
+  y: -0.05,
 });
-const groundMesh = new THREE.Mesh(groundGeometry, groundMaterial);
-groundMesh.rotation.x = THREE.MathUtils.degToRad(-90);
-groundMesh.receiveShadow = true;
 
-scene.add(groundMesh);
-
-camera.lookAt(boxMesh.position);
+const floor = new MeshObject({
+  scene,
+  name: "floor",
+  width: 5,
+  height: 0.4,
+  depth: 5,
+});
 
 // draw
 // 기기마다 함수 실행 간격 맞추기 - clock 사용
 const clock = new THREE.Clock();
 function draw() {
-  //   boxMesh.position.y += 0.01;
-  const delta = clock.getDelta();
-  boxMesh.position.y += delta;
-  if (boxMesh.position.y > 5) {
-    boxMesh.position.y = 0;
-  }
-  controls.update();
   renderer.render(scene, camera);
   window.requestAnimationFrame(draw);
 }
